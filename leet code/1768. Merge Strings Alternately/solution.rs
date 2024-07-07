f9bdac9fef0259b6bdb87dadda496ee697a04366
dirk122119 <dirk122119@gmail.com>
2024-07-07 15:38:47 +0800

    fix: 🐛 edit 1768

--- leet code/1768. Merge Strings Alternately/solution.rs
+++ leet code/1768. Merge Strings Alternately/solution.rs
@@ -2,30 +2,22 @@ struct Solution;
 
 impl Solution {
     pub fn merge_alternately(word1: String, word2: String) -> String {
-        // let mut rs = String::new(); runtime beats 68%
-        let mut rs = String::with_capacity(word1.len() + word2.len());//runtime beats 100%
-        let mut w1_ptr = 0;
-        let mut w2_ptr = 0;
+        //let mut rs = String::new();
+        let mut rs = String::with_capacity(word1.len() + word2.len());
+        let mut w1_iter = word1.chars();
+        let mut w2_iter = word2.chars();
         loop{
-            if w1_ptr<word1.len() && w2_ptr<word2.len() {
-                rs.push_str(&word1[w1_ptr..w1_ptr+1]);
-                rs.push_str(&word2[w2_ptr..w2_ptr+1]);
-                w1_ptr +=1;
-                w2_ptr +=1;
-            } else if w1_ptr<word1.len() {
-                rs.push_str(&word1[w1_ptr..w1_ptr+1]);
-                w1_ptr +=1;
-            } else if w2_ptr<word2.len(){
-                rs.push_str(&word2[w2_ptr..w2_ptr+1]);
-                w2_ptr +=1;
-            } else{
-                break;
-            } 
+            match(w1_iter.next(),w2_iter.next()) {
+                (Some(c1),Some(c2)) => {
+                    rs.push(c1);
+                    rs.push(c2);
+                },
+                (Some(c),None) | (None,Some(c)) => rs.push(c),
+                (None,None) => return rs
+            }
         }
-        rs
     }
 }
-
 fn main() {
     let word1 = String::from("abc");
     let word2 = String::from("pqr");
